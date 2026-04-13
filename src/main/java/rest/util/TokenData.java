6815add238350b68d473df5a39dbc581f740cfef
Dinis-Neves <dinis15neves@gmail.com>
2026-04-13 10:54:50 +0100

    new commit amended endpoints and token input

--- src/main/java/rest/util/TokenData.java
+++ src/main/java/rest/util/TokenData.java
@@ -5,6 +5,11 @@ import java.util.UUID;
 public class TokenData {
 
     public String tokenId;
+    public String username;
+    public String role;
+    public long issuedAt;
+    public long expiresAt;
+
 
     public TokenData(){ }
 
